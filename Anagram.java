aa1fee0444405abfc0682bde3fff81d1151b2520
ramakrishna2707 <115349199+ramakrishna2707@users.noreply.github.com>
2024-01-16 17:39:23 +0530

    Anagram

---
+++ Anagram.java
@@ -0,0 +1,33 @@
+package Ramlogin;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String a="act";
+       String b="c"
+       		+ "at";
+       char[]a1=a.toCharArray();
+       char[]b1=b.toCharArray();
+       int count=0;
+       if(a1.length==b1.length) {
+    	   for(int j=0;j<b1.length;j++) {
+    		   char ch=b1[j];
+    		   for(int i=0;i<a1.length;i++) {
+    			   if(ch==a1[i]) {
+    				   a1[i]='\n';
+    				   count++;
+    				   break;   
+    			   }
+    		   }
+    	   }
+    	   if(count==b.length()) {
+    		   System.out.println("Anagram");
+    	   }
+    	   else {
+    		   System.out.println("Not Anagram");
+    	   }
+       }
+	}
+
+}
